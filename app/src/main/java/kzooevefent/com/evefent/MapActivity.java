4726912ebd06e90c828715117aabfb4a16def183
Miles McDowall <mlm1000@comcast.net>
2015-10-26 11:25:59 -0400

    first iteration of Google Maps integration

---
+++ app/src/main/java/kzooevefent/com/evefent/MapActivity.java
@@ -0,0 +1,62 @@
+package kzooevefent.com.evefent;
+
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+
+
+public class MapActivity extends FragmentActivity
+            implements OnMapReadyCallback{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        //Initiates a google maps sync.
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+        MapFragment mapFragment = (MapFragment) getFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_map, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+    @Override
+    public void onMapReady(GoogleMap map) {
+        //sets a marker at kalamazoo college and moves the camera there.
+        LatLng test = new LatLng(42.2900, 85.6010);
+        map.addMarker(new MarkerOptions()
+                .position(test)
+                .title("Kalamazoo College"));
+        map.moveCamera(CameraUpdateFactory.newLatLng(test));
+
+    }
+}
