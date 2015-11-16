05eb38f4731e2e09b1c5bc331def3014b6dbcef0
Emily Fletcher <EFletcher2014@gmail.com>
2015-11-16 11:32:30 -0500

    Added my AttendeeList stuff

--- app/src/main/java/kzooevefent/com/evefent/AttendeeList.java
+++ app/src/main/java/kzooevefent/com/evefent/AttendeeList.java
@@ -7,6 +7,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.ServiceConnection;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.support.v4.content.LocalBroadcastManager;
@@ -18,9 +19,6 @@ import android.widget.TableRow;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.facebook.CallbackManager;
-import com.facebook.FacebookSdk;
-
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -31,11 +29,8 @@ import java.util.ArrayList;
  * Created by Emily on 11/12/2015.
  */
 public class AttendeeList extends Activity {
-     int ID;
-    DatabaseServices dbService; //The database service instance the activity binds to.
+    int ID;
     boolean dbBound = false; //Is the activity bound to the service
-    EventProfile eve;
-
     /*private ServiceConnection dbConnection = new ServiceConnection()
     {
 
@@ -86,7 +81,7 @@ public class AttendeeList extends Activity {
         *        new IntentFilter(getApplicationContext().getResources().getString(R.string.EventGPSUpdateMessageEnumeratedMessage)));
         * */
         //LocalBroadcastManager.getInstance(this).registerReceiver(DatabaseProfileEnumerationMessageReceiver,
-          //      new IntentFilter(getApplicationContext().getResources().getString(R.string.EventProfilesEnumeratedMessage)));
+        //      new IntentFilter(getApplicationContext().getResources().getString(R.string.EventProfilesEnumeratedMessage)));
     }
 
     @Override
@@ -102,18 +97,16 @@ public class AttendeeList extends Activity {
         //bindService(intent, dbConnection, Context.BIND_AUTO_CREATE);
         BufferedReader buf;
         ArrayList<String> lines = new ArrayList<String>();
+        System.out.println("Starting");
         lines.add("Tommy Pickles");
-        lines.add("Dil Pickles");
         lines.add("Chuckie Finster");
         lines.add("Phil Deville");
         lines.add("Lil Deville");
-        lines.add("Angelica Pickles");
-
-        System.out.println("Starting");
-
+        //lines.add("Angelica Pickles");
+        //lines.add("Dil Pickles");
         /*try
         {
-            buf = new BufferedReader(new FileReader("assets\\AttendeeList.txt"));
+            buf = new BufferedReader(new FileReader("AttendeeList.txt"));
             try
             {
                 String temp="";
@@ -135,23 +128,44 @@ public class AttendeeList extends Activity {
         }*/
 
         //EventProfile eve = updateEventProfile(ID);
-
+        TextView text;
         TableRow row;
 
         TableLayout table = (TableLayout) findViewById(R.id.list_table);
+        row = new TableRow(this);
+        row.setBackgroundColor(Color.parseColor("#230439"));
+        text = new TextView(this);
+        text.setText("Attendees: ");
+        text.setTextSize(24);
+        text.setTextColor(Color.parseColor("#6F5384"));
+        row.addView(text);
+        table.addView(row, 0);
+
 
         for (int i = 0; i < lines.size(); i++) {
-
-            LayoutInflater inflater = LayoutInflater.from(AttendeeList.this);
-            row = (TableRow) inflater.inflate(R.layout.my_row, null);
-            System.out.println("row: " + row);
+            row = new TableRow(this);
+            text = new TextView(this);
+            //LayoutInflater inflater = LayoutInflater.from(AttendeeList.this);
+            //row = (TableRow) inflater.inflate(R.layout.my_row, null);
             final String name = lines.get(i);
-
-            setContentView(R.layout.my_row);
-            TextView text = (TextView) findViewById(R.id.myText);
-            System.out.println("text: " + text);
+            //setContentView(R.layout.my_row);
+            //TextView text = (TextView) findViewById(R.id.myText);
             text.setText(name);
-            table.addView(row);
+            text.setTextColor(Color.parseColor("#8F8595"));
+            text.setTextSize(18);
+            row.addView(text);
+            table.addView(row, i+1);
         }
+    }
+
+    public void addName(View view)
+    {
+        EditText names;
+        names = (EditText) findViewById(R.id.Name);
+        String name = names.getText().toString();
+        names.setBackgroundColor(Color.parseColor("#734C8F"));
+
+        final Toast addNameToast = Toast.makeText(getApplicationContext(), "Add " + name + " to list", Toast.LENGTH_LONG);
+        addNameToast.show();
     }
 }
